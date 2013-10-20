e44563f32c6881918b9d0b8825d58a3175e5bd97
Ryan Skoblenick <ryan@skoblenick.com>
2013-10-20 06:23:01 -0400

    Corrected commented out default and removed notice.

--- manifests/install.pp
+++ manifests/install.pp
@@ -15,11 +15,9 @@ class java::install {
   $url_hash = $java::url_hash
   $version = $java::version
 
-  notice(downcase($::osfamily))
-
   $source = downcase($::osfamily) ? {
     'darwin' => "http://supportdownload.apple.com/download.info.apple.com/Apple_Support_Area/Apple_Software_Updates/Mac_OS_X/downloads/${url_hash}/JavaForOSX${version}.dmg",
-    #default => fail("Unsupported Kernel: ${::kernel} operatingsystem: ${::operatingsystem}")
+    default => fail("Unsupported Kernel: ${::kernel} operatingsystem: ${::operatingsystem}")
   }
 
   package {"JavaForOSX${version}":
